9922b7c5315c6d9a627abc99b1af7a6d5057fe2d
SpidY21 <yashkumarroy164@gmail.com>
2023-07-02 23:10:08 +0530

    Final Commit

--- src/main/java/com/example/temperatureconvertertooljava/HelloApplication.java
+++ src/main/java/com/example/temperatureconvertertooljava/HelloApplication.java
@@ -1,23 +1,75 @@
 package com.example.temperatureconvertertooljava;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        VBox rootNode = loader.load();
+        MenuBar menuBar= createMenu();
+        rootNode.getChildren().add(0,menuBar);
+        Scene scene = new Scene(rootNode, 300, 275);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Temperature Convertor Tool");
+//        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    private MenuBar createMenu(){
+        Menu fileMenu= new Menu("File");
+        MenuItem newMenuItem= new MenuItem("new");
+        SeparatorMenuItem separatorMenuItem=new SeparatorMenuItem();
+        MenuItem quitMenuItem= new MenuItem("Quit");
+        fileMenu.getItems().addAll(newMenuItem,separatorMenuItem,quitMenuItem);
+        newMenuItem.setOnAction(actionEvent -> System.out.println("Creating New File"));
+        quitMenuItem.setOnAction(actionEvent -> {
+            System.out.println("Exiting...");
+            Platform.exit();
+//                System.exit(0);
+        });
+
+        Menu helpMenu=new Menu("Help");
+        MenuItem aboutApp=new MenuItem("About");
+        helpMenu.getItems().addAll(aboutApp);
+
+        aboutApp.setOnAction(actionEvent -> aboutApp());
+
+        MenuBar menuBar=new MenuBar();
+        menuBar.getMenus().addAll(fileMenu,helpMenu);
+        return menuBar;
+    }
+
+    private void aboutApp() {
+        System.out.println("Opening about");
+        Alert alertDialog = new Alert(Alert.AlertType.INFORMATION);
+        alertDialog.setTitle("Title");
+        alertDialog.setHeaderText("Header");
+        alertDialog.setContentText("This is content");
+
+        ButtonType yesBtn=new ButtonType("Yes");
+        ButtonType noBtn=new ButtonType("No");
+        alertDialog.getButtonTypes().setAll(yesBtn,noBtn);
+
+        Optional<ButtonType> clickedBtn=alertDialog.showAndWait();
+        if(clickedBtn.get()==yesBtn){
+            System.out.println("Yes");
+        }
+        if(clickedBtn.get()==noBtn){
+            System.out.println("No");
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/example/temperatureconvertertooljava/HelloController.java
+++ src/main/java/com/example/temperatureconvertertooljava/HelloController.java
@@ -1,14 +1,73 @@
 package com.example.temperatureconvertertooljava;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Label WelcomeLable;
+    @FXML
+    public ChoiceBox<String> ChoiceBox;
+    @FXML
+    public TextField txtField;
+    @FXML
+    public Button Btn;
+
+    private boolean isTrue = true;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ChoiceBox.getItems().add("Celsius to Fahrenheit");
+        ChoiceBox.getItems().add("Fahrenheit to Celsius");
+        ChoiceBox.setValue("Celsius to Fahrenheit");
+        ChoiceBox.getSelectionModel().selectedItemProperty().addListener((observableValue, s, t1) -> isTrue = t1.equals("Celsius to Fahrenheit"));
+        Btn.setOnAction(actionEvent -> {
+//            System.out.println("button Clicked");
+            convert();
+        });
+
+    }
+
+    private void convert() {
+        String input = txtField.getText();
+        float temp = 0.0f;
+        float newTemp = 0.0f;
+        try {
+            temp = Float.parseFloat(input);
+            if (isTrue) {
+                newTemp = (temp * 9 / 5) + 32;
+            } else {
+                newTemp = (temp - 32) * 5 / 9;
+            }
+            if (isTrue) {
+                newTemp = (temp * 9 / 5) + 32;
+            } else {
+                newTemp = (temp - 32) * 5 / 9;
+            }
+            display(newTemp);
+        } catch (Exception e) {
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            error.setTitle("Error");
+            error.setHeaderText("Some Error has occurred");
+            error.setContentText("Please Enter valid input!!");
+            error.show();
+        }
+    }
+
+    private void display(float newTemp) {
+        String unit = isTrue ? "F" : "C";
+        System.out.println("The converted Temp is: " + newTemp + unit);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Result");
+        alert.setHeaderText("This is your answer.");
+        alert.setContentText("The converted Temp is: " + newTemp + unit);
+        alert.show();
     }
 }
